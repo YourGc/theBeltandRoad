583ef76b3dcc7230b683b336f157e44b4e4b3bda
52757 <527573174@qq.com>
2019-06-01 20:32:13 +0800

    modify

--- dataset.py
+++ dataset.py
@@ -67,7 +67,7 @@ class custom_Dataset(Dataset):
             img_names = os.listdir(root_path)
             for img_name in img_names:
                 data['x'].append(os.path.join(root_path,img_name))
-                data['y'].append(np.array(int(label)))
+                data['y'].append(np.array([int(label)]))
 
         data['x'] = np.array(data['x'])
         data['y'] = np.array(data['y'])
@@ -91,6 +91,7 @@ class custom_Dataset(Dataset):
     def __getitem__(self,index):
         img = torch.from_numpy(self.get_img(index))
         lable = torch.from_numpy(self.data['y'][index])
+
         return img,lable
 
     def __len__(self):
