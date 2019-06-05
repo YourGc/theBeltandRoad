7e117f9740ec26e4af8ab95c8eb381db9f8108ca
52757 <527573174@qq.com>
2019-06-05 14:02:49 +0800

    add

--- loss.py
+++ loss.py
@@ -3,34 +3,52 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class CELoss(nn.Module):
-    def __init__(self,weight = None):
-        super(CELoss,self).__init__()
-        self.gamma = 2
-        self.alpha = 0.1
-    def forward(self, pred,target):
-        '''
-        :param pre: logit value output by CNN
-        :param target: label
-        :return: CELoss
-        '''
-        print(pred[0])
-        print(target[0])
-        probs = F.softmax(pred,1)
-        print(probs[0])
-        print('compute')
-        print((probs ** self.gamma * self.alpha) [0] )
-        print(torch.log(1-probs)[0])
-        #torch.log是以e为底数的ln
-        loss = - (self.alpha) * ( probs ** self.gamma ) * torch.log(1 - probs)
-        print(loss[0])
-        print(target.shape)
-        print(probs[target].shape,probs[target][0])
-        loss[target] = - (1 - self.alpha) * (probs[target] ** self.gamma) * torch.log(probs)
-        print(loss[0])
-        exit(0)
+from torch.autograd import Variable
 
-        return loss.sum()
+class CELoss(nn.Module):
+    def __init__(self, class_num, alpha=None, gamma=2, size_average=True):
+        super(CELoss, self).__init__()
+        if alpha is None:
+            self.alpha = Variable(torch.ones(class_num, 1))
+        else:
+            if isinstance(alpha, Variable):
+                self.alpha = alpha
+            else:
+                self.alpha = Variable(alpha)
+        self.gamma = gamma
+        self.class_num = class_num
+        self.size_average = size_average
+
+    def forward(self, inputs, targets):
+        N = inputs.size(0)
+        C = inputs.size(1)
+        P = F.softmax(inputs)
+
+        class_mask = inputs.data.new(N, C).fill_(0)
+        class_mask = Variable(class_mask)
+        ids = targets.view(-1, 1)
+        class_mask.scatter_(1, ids.data, 1.)
+        # print(class_mask)
+
+        if inputs.is_cuda and not self.alpha.is_cuda:
+            self.alpha = self.alpha.cuda()
+        alpha = self.alpha[ids.data.view(-1)]
+
+        probs = (P * class_mask).sum(1).view(-1, 1)
+
+        log_p = probs.log()
+        # print('probs size= {}'.format(probs.size()))
+        # print(probs)
+
+        batch_loss = -alpha * (torch.pow((1 - probs), self.gamma)) * log_p
+        # print('-----bacth_loss------')
+        # print(batch_loss)
+
+        if self.size_average:
+            loss = batch_loss.mean()
+        else:
+            loss = batch_loss.sum()
+        return loss
 
 
 
