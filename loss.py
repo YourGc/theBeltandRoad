31590ae3e3e7e7c794a6c16fd4a24dbbb8505a02
52757 <527573174@qq.com>
2019-06-05 12:49:59 +0800

    add

--- loss.py
+++ loss.py
@@ -8,14 +8,14 @@ class CELoss(nn.Module):
         super(CELoss,self).__init__()
         self.gamma = 2
         self.alpha = 0.1
-        self.nll_loss = torch.nn.NLLLoss2d(weight)
+        self.nll_loss = torch.nn.NLLLoss(weight)
     def forward(self, pred,target):
         '''
         :param pre: logit value output by CNN
         :param target: label
         :return: CELoss
         '''
-        probs = F.softmax((pred,1))
+        probs = F.softmax(pred,1)
         return self.nll_loss(self.alpha * (1 - probs) ** self.gamma * probs, target)
 
 
