b0888dce67d576e0133c9e5a5e118180a79bbd7d
52757 <527573174@qq.com>
2019-06-05 11:57:09 +0800

    add

--- loss.py
+++ loss.py
@@ -17,24 +17,21 @@ class CELoss(nn.Module):
         alpha = 0.1
         gamma = 2
 
-        #数值稳定的softmax
-        print(pred[0])
-        out_max,_ = torch.max(pred,1)
-        print(out_max[0],_[0])
-        pred -= out_max
-        print(pred[0])
-        pred = torch.exp(pred)
-        print(pred[0])
-        pros = pred/torch.sum(pred,0)
+        #softmax
+        print('---pred')
+        print(pred)
+        pros = F.softmax(pred)
+        p_max,idx = torch.max(pred,0)
 
-        print(pros[0])
-        p_out,idx = torch.max(pros,0)
-        print(p_out[0])
+        print("--pros--")
+        print(pros)
+        print("--pmax--")
+        print(p_max)
 
-        neg_pros = pros[pros == p_out]
-        print(neg_pros[0])
-        pos_pros = pros[pros != p_out]
-        print(pos_pros[0])
+        neg_pros = pros[pros == p_max]
+        #print(neg_pros[0])
+        pos_pros = pros[pros != p_max]
+        #print(pos_pros[0])
 
         neg_loss = -(1-alpha) * (neg_pros ** gamma) * torch.log(1-neg_pros)
         pos_loss = -alpha * ((1 - pos_pros) ** gamma) * torch.log(pos_pros)
