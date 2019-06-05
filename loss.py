86a5704ee50ac9d30207013ab399050f8e7c6107
52757 <527573174@qq.com>
2019-06-05 13:09:57 +0800

    add

--- loss.py
+++ loss.py
@@ -14,9 +14,14 @@ class CELoss(nn.Module):
         :param target: label
         :return: CELoss
         '''
+        print(target)
         probs = F.softmax(pred,1)
+        print(probs[0])
         loss = - (self.alpha) * ( probs ** self.gamma ) * torch.log(1 - probs)
+        print(loss[0])
         loss[target] = - (1 - self.alpha) * (probs[target] ** self.gamma) * torch.log(probs)
+        print(loss[0])
+        exit(0)
 
         return loss.sum()
 
